9729b9c82843143af5c7101ba251a852ffd9fa17
Jeremy Hughes <jedahu@gmail.com>
2012-05-19 22:38:13 +1200

    license and description

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject domicile "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A clojurescript DOM library"
+  :url "https://github.com/jedahu/domicile"
+  :license {:name "ISC"
+            :url "http://opensource.org/licenses/isc-license.txt"}
 
   :profiles
   {:dev
