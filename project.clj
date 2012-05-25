b73c5416d28e984c518933ff76aa11cfa13dd3a8
Jeremy Hughes <jedahu@gmail.com>
2012-05-25 18:28:25 +1200

    next snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject domicile "0.4.0"
+(defproject domicile "0.4.1-SNAPSHOT"
   :description "A clojurescript DOM library"
   :url "https://github.com/jedahu/domicile"
   :license {:name "ISC"
